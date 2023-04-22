b5f738d80c30dc656111ac6da8db5a4e567c86ea
srajankumar <kumarsrajan02@gmail.com>
2023-04-22 14:22:37 +0530

    links for arts

--- src/components/Arts.jsx
+++ src/components/Arts.jsx
@@ -13,16 +13,27 @@ import img10 from "../arts/10.jpeg";
 import img11 from "../arts/11.jpg";
 import img12 from "../arts/12.jpg";
 
+import instagram from "../icons/instagram-white.png";
+
 function Arts() {
   return (
     <div id="arts">
       <header>
-        <h4 className="home">
+        <h4 id="home">
           <Link className="home" to="/">
             back
           </Link>
         </h4>
+        <nav>
+          <a href="https://instagram.com/reoarts/" target="_blank">
+            <img id="iconn-insta" src={instagram} />
+          </a>
+          <a href="https://twitter.com/reoarts/" target="_blank">
+            <i id="iconn" class="devicon-twitter-original"></i>
+          </a>
+        </nav>
       </header>
+
       <h2 id="artshead">Reo Arts</h2>
       <div className="artsparent">
         <div>
